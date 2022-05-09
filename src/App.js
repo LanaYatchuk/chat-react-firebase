4600fb04a31883745275c1d95e1889da403a24dc
Svitlana <yatchuksvitlana1@gmail.com>
2022-05-09 17:20:36 +0300

    new update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './styles/App.css'
 
 function App() {
   const {auth} = useContext(Context)
-  const [user, loading, error] = useAuthState(auth)
+  const [loading] = useAuthState(auth)
 
   if (loading){
     return <Loader/>
